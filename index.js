c51a960cd073864d0f240e19c425402ea7a13a38
EngineerVirajPatil <engineer.virajpatil@gmail.com>
2024-11-17 12:01:31 +0530

    Push 1

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const { resolve } = require('path');
 const app = express();
 const port = 3000;
 
+let numbers=[1,2,3,4,5,6,7,8,9,10,11,12];
+
+function filterEvenNumber(num){
+  return num%2===0;
+}
+
+app.get('/even-numbers',(req, res)=>{
+  let result=numbers.filter((num)=>filterEvenNumber(num));
+  res.json(result);
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
